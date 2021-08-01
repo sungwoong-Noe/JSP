c2e5e143c95fb8009caa3d31bfd459541c9decd2
Noe <asdz453@gmail.com>
2021-08-01 13:57:12 +0900

    회원 등록 폼

---
+++ src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
@@ -0,0 +1,42 @@
+package hello.servlet.web.servlet;
+
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/member/new-form")
+public class MemberFormServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance(); //MemberRepository 인스턴스 가져오기
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //HTTP 메시지 응답으로 HTML이 나가야함 -> content body를 잡아줘야함
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+        //HTML 코드
+        PrintWriter w = response.getWriter();
+        w.write("<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "   <meta charset=\"UTF-8\">\n" +
+                "   <title>Title</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<form action=\"/servlet/members/save\" method=\"post\">\n" +
+                "   username: <input type=\"text\" name=\"username\" />\n" +
+                "   age: <input type=\"text\" name=\"age\" />\n" +
+                "   <button type=\"submit\">전송</button>\n" +
+                "</form>\n" +
+                "</body>\n" +
+                "</html>\n");
+    }
+}
